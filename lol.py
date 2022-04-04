4b75f8d760b9d1b54ecae68e50f1c304396acecd
ignaciobarbieri <ignaciobarbieri2003@gmail.com>
2022-04-03 22:14:48 -0300

    el phytonnnnnnnnn

---
+++ lol.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+#PROFE SI ESTA LEYENDO ESTO LE INFORMO QUE EL RESULTADO DE LA API ES LA IMAGEN DE UN PERRO PERO COMO LA ACTIVIDAD ERA UN .TXT SE LO MANDO ASI SI QUIERE VER AL PERRO DIGAME
+
+if __name__ == '__main__':
+    url = ' https://images.dog.ceo/breeds/terrier-dandie/n02096437_48.jpg'
+    chapela = requests.get(url)
+
+    if chapela.status_code == 200:
+        content = chapela.content
+
+        file = open('esunperro.txt','wb')
+        file.write(content)
+        file.close()
